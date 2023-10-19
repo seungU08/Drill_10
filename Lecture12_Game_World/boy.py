32141ee467f244dc36152b9cdd8756e1b705d702
SWnotebook <sungwoo5957@naver.com>
2023-10-19 10:46:29 +0900

    소년의 위치를 아래로 내림

--- Lecture12_Game_World/boy.py
+++ Lecture12_Game_World/boy.py
@@ -146,7 +146,7 @@ class StateMachine:
 
 class Boy:
     def __init__(self):
-        self.x, self.y = 400, 90
+        self.x, self.y = 400, 50
         self.frame = 0
         self.action = 3
         self.dir = 0
